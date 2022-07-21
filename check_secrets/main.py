1ee1bb9933b9bf50b5e1186be187854d6fee55e7
sohel-tamboli <sohel.tamboli@medly.com>
2022-07-21 18:13:25 +0530

    changed location

--- check_secrets/main.py
+++ check_secrets/main.py
@@ -1,10 +1,16 @@
 # print_arguments/main.py
 import argparse
-
+import os
 
 def print_arguments(arguments: list[str]):
+    print(os.system("pwd"))
+    print(os.system("ls"))
+    # os.system("brew tap trufflesecurity/trufflehog")
+    # os.system("brew install trufflehog")
+    
     for argument in arguments:
-        print(argument)
+        pass
+        # print(argument)
 
 
 def main():
