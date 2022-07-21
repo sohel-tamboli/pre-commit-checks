ee6a9f3d6c498755c8e4d6ee06c75c1ab4bd6706
sohel-tamboli <sohel.tamboli@medly.com>
2022-07-21 18:33:33 +0530

    changed

--- build/lib/check_secrets/main.py
+++ build/lib/check_secrets/main.py
@@ -3,22 +3,21 @@ import argparse
 import os
 
 def print_arguments(arguments: list[str]):
-    print(os.system("pwd"))
-    # print(os.system("ls"))
+    # print(os.system("pwd"))
+    # print(os.system("ls")
     # os.system("brew tap trufflesecurity/trufflehog")
     # os.system("brew install trufflehog")
     
     for argument in arguments:
-        # pass
         print(argument)
 
 
 def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("filenames", nargs="*")
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", nargs="*")
+    args = parser.parse_args()
 
-    print_arguments(["args.filenames"])
+    print_arguments(args.filenames)
 
 
 if __name__ == "__main__":
